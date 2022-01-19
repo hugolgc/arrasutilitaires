9f4dcb76118e00fad8da1fdc7042bfd5092ce35d
hugolgc <lagache.hugo@hotmail.fr>
2022-01-19 14:23:21 +0100

    Api Origin

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,8 @@ import { createStore } from "vuex";
 
 export default createStore({
   state: {
-    // url: "http://localhost:1337",
-    // url: "https://arrasutilitairesapi.ddns.net",
-    url: "https://gyis3d.n0c.world/",
+    url: "http://localhost:1337",
+    // url: "https://gyis3d.n0c.world/",
     token: null,
     user: null,
     header: [],
